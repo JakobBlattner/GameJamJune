917bd56a18bcc340ae6c91fd2add55802ceb02d7
JakobBlattner <Jakoblattner@gmail.com>
2018-06-30 20:22:51 +0200

    added parameter bars (health, shield, etc.)

---
+++ Assets/ParameterVisualizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParameterVisualizer : MonoBehaviour {
+
+    public Slider healthBar;
+    public Slider shieldBar;
+    public Slider powerBar;
+    public Slider steamBar;
+
+    // Use this for initialization
+    void Start () {
+        if (healthBar == null || shieldBar == null || powerBar == null || steamBar == null)
+            Debug.Log("Not all parameter bars set in script. Add via Editor");
+	}
+	
+	void LateUpdate () {
+        healthBar.value = 0; //Ship.Health;
+        shieldBar.value = 0; //Ship.Shield;
+        powerBar.value = 0; //Ship.Power;
+        steamBar.value = 0; //Ship.Steam;
+	}
+}
