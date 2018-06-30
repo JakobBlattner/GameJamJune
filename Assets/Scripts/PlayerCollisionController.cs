6a7d398e2cfc4107780721d2c4ae5295ecb0a615
Lukas Schaper <lukas86.schaper@gmail.com>
2018-06-30 22:28:48 +0200

    Closeable Doors added

--- Assets/Scripts/PlayerCollisionController.cs
+++ Assets/Scripts/PlayerCollisionController.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class PlayerCollisionController : MonoBehaviour {
+public class PlayerCollisionController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 
     void OnTriggerEnter(Collider other)
     {
-//        Destroy(other.gameObject);
+        //        Destroy(other.gameObject);
     }
+
 }
