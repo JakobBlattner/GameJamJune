e9554da0456e8a2503050d5437fbceac88fb1c32
Konrad <konrad.weinhandl@gmail.com>
2018-06-30 14:32:48 +0200

    MaximumVelocity hinzu; Player bleibt stehen, wenn kein weiterer Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public float Speed;
+    public float MaximumVelocity;
 
-    private Rigidbody2D rb2d;
+    private Rigidbody2D _body;
 
     public GameObject Player
     {
@@ -17,10 +18,33 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        _body = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
+    {
+        var movement = GetMovement();
+
+        if(movement == Vector2.zero)
+            RemoveForce(_body);
+        else
+            AddMovementToForce(movement, _body);
+        
+    }
+
+    static void RemoveForce(Rigidbody2D body)
+    {
+        body.velocity = Vector2.zero;
+    }
+
+    private void AddMovementToForce(Vector2 movement, Rigidbody2D body)
+    {
+        ClampMaximumSpeed(body);
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        body.AddForce(movement * Speed);
+    }
+
+    private static Vector2 GetMovement()
     {
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -30,9 +54,13 @@ public class PlayerController : MonoBehaviour
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        return movement;
+    }
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * Speed);
+    private void ClampMaximumSpeed(Rigidbody2D body)
+    {
+        var velocityMagnitude = body.velocity;
+        body.velocity = Vector2.ClampMagnitude(velocityMagnitude, MaximumVelocity);
     }
 
     // Update is called once per frame
