d9b3897c7eec7ee42f6a6502ee611a54303ded92
JakobBlattner <Jakoblattner@gmail.com>
2018-07-01 18:34:37 +0200

    added shield. opacity according to shield value

---
+++ Assets/Scripts/ShieldVisualizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldVisualizer : MonoBehaviour {
+
+    private float shieldValue;
+    private SpriteRenderer spr;
+
+    void Start () {
+        spr = this.GetComponent<SpriteRenderer>();
+    }
+	
+    //changes opacity of shield according to current shield value
+	void LateUpdate () {
+        shieldValue = Ship.Instance.Shield;
+        spr.color = new Color(255, 255, 255, shieldValue / 100);
+    }
+}
