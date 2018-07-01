4d9afa5d99ca16c3201bd5b53093148c36fcf8c5
JakobBlattner <Jakoblattner@gmail.com>
2018-07-01 15:50:28 +0200

    added fire to engine and set sideview of ship in overview

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
     void FixedUpdate()
     {
         var ship = GetShip();
@@ -19,9 +14,4 @@ public class ShipController : MonoBehaviour {
     {
         return Ship.Instance;
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
